62bfb115fddd39a08fb8754a4080156f45243d8f
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-09-11 12:28:32 +0900

    공통 코드 가져오기

--- MatZip/src/com/koreait/matzip/restaurant/RestaurantController.java
+++ MatZip/src/com/koreait/matzip/restaurant/RestaurantController.java
@@ -2,6 +2,7 @@ package com.koreait.matzip.restaurant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.koreait.matzip.CommonDAO;
 import com.koreait.matzip.Const;
 import com.koreait.matzip.ViewRef;
 
@@ -14,7 +15,8 @@ public class RestaurantController {
 	}
 	
 	public String restReg(HttpServletRequest request) {
-		request.setAttribute("categoryList", null);
+		final int I_M = 1; //카테고리 코드
+		request.setAttribute("categoryList", CommonDAO.selCodeList(I_M));
 		
 		request.setAttribute(Const.TITLE, "가게 등록");
 		request.setAttribute(Const.VIEW, "restaurant/restReg");
